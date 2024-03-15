14b446fd56b023ae2ef0716d6abf737f55c5a238
Gwendal Le Vaillant <glevaillant@he2b.be>
2024-03-15 10:50:42 +0100

    Extra amplitude norm if clipping + morphing error detection

--- morphing_sequences.m
+++ morphing_sequences.m
@@ -5,8 +5,9 @@ clear all;
 root_data_dir = 'Z:/STRAIGHT_48000Hz_morphing9_test/';
 n_steps = 9;
 morph_factors = 0.0:(1/(n_steps-1)):1.0;
+n_morph_seq_with_errors = 0;
 
-for seq_index=0:1514
+for seq_index=207:1514  % 0:1514
     disp(['===== Sequence ' num2str(seq_index) ' =====']);
     
     seq_dir = [root_data_dir num2str(seq_index,'%05d') '/'];
@@ -16,8 +17,11 @@ for seq_index=0:1514
     target=createMobject;
     [x,fs] = audioread([seq_dir 'start.wav']);
     source=updateFieldOfMobject(source,'waveform',x);
-    [x,fs] = audioread([seq_dir 'end.wav']);
-    target=updateFieldOfMobject(target,'waveform',x);
+    [x2,fs2] = audioread([seq_dir 'end.wav']);
+    target=updateFieldOfMobject(target,'waveform',x2);
+    if (length(x) ~= length(x2)) || (fs ~= fs2)
+        error('Morphing inputs must have the same length and sampling frequency');
+    end
     
     % Analysis
     tic
@@ -29,13 +33,40 @@ for seq_index=0:1514
     % Morphing: only the 3rd solution is viable
     % Others need some manual time alignment
     tic
+    n_steps_with_error = 0;  % number of morphing that have failed
     for i=1:n_steps
-        mObject = directSTRAIGHTmorphing(source,target, morph_factors(i), 'linear');
-        % / 32768 alone: still leads to a clipping warnings
-        sy_morphed = 0.99 * executeSTRAIGHTsynthesisM(mObject) / 32768;
+        try
+            % Error detection (e.g. in sequence 207... incompatible shapes)
+            mObject = directSTRAIGHTmorphing(source,target, morph_factors(i), 'linear');
+            sy_morphed = executeSTRAIGHTsynthesisM(mObject);
+            morphing_error = false;
+        catch exception
+            morphing_error = true;
+            disp(exception);
+        end
+        if morphing_error == false
+            % / 32768 alone: still occasionaly leads to a clipping (morphing 'diverges')
+            sy_morphed = 0.99 * sy_morphed / 32768.0;
+            if max(abs(sy_morphed)) > 0.99
+                sy_morphed = 0.99 * sy_morphed / max(abs(sy_morphed));
+            end
+        else  % morphing error did happen
+            warning(['MORPHING ERROR - sequence ' num2str(seq_index) ' step ' num2str(i-1)]);
+            n_steps_with_error = n_steps_with_error + 1;
+            % write a null audio file
+            sy_morphed = x * 0.0;
+        end
         audiowrite([seq_dir 'audio_step' num2str(i-1, '%02d') '.wav'], sy_morphed, fs);
+    end
+    if n_steps_with_error == n_steps
+        error(['All steps from morphing sequence ' num2str(seq_index) ' have failed']);
+    elseif n_steps_with_error > 0
+        n_morph_seq_with_errors = n_morph_seq_with_errors + 1;
+        warning(['n_morph_seq_with_errors = ' num2str(n_morph_seq_with_errors)]);
     end
     disp("Resynthesis duration (TOTAL):");
     toc
     
 end
+
+disp(['========== End. n_morph_seq_with_errors = ' num2str(n_morph_seq_with_errors) ' ==========']);
